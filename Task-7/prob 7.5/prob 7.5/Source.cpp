d8819e85dfdfc4785d705789067cdb7b320eebdc
salma3rafa <103216606+salma3rafa@users.noreply.github.com>
2022-12-16 08:35:06 +0200

    problem 5 done

---
+++ Task-7/prob 7.5/prob 7.5/Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int d1, d2, m1, m2, y1, y2, diffDays, diffMon, diffYears;
+	long long fine = 0;
+	cin >> d1 >> m1 >> y1;
+	cin >> d2 >> m2 >> y2;
+	if (d1 > d2 && m1 == m2 && y1 == y2)
+	{
+		diffDays = abs(d2 - d1);
+		fine += (diffDays * 15);
+	}
+	else if (m1 > m2 && y1 == y2)
+	{
+		diffMon = abs(m2 - m1);
+		fine += (diffMon * 500);
+	}
+	else if (y1 > y2)
+	{
+		diffYears = abs(y2 - y1);
+		fine += (diffYears * 10000);
+	}
+	cout << fine << endl;
+
+	return 0;
+}
